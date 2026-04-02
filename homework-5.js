398b34bb187da3626a5dfa32e0447d115d008802
devgai34-gif <devgai34@gmail.com>
2026-04-02 15:42:59 +0300

    Remade-DZ

--- homework-5.js
+++ homework-5.js
@@ -1,71 +1,60 @@
-//3. Создать функцию, которая принимает 2 параметра: город и температуру 
+//3. Создать функцию, которая принимает 2 параметра: 
+// город и температуру
 
-function showWeather(city, temperature) {
-  console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
+function weather(city,temperature) {
+  console.log(" в " + city + " t* — " + temperature + " градус");
 }
 
-// Пример вызова функции:
-showWeather('RIM', 20);
-showWeather('STAMBUL', 15);
+weather("Грозном",1);
 
-//4. Создать переменную, которая хранит внутри себя скорость света
-// Константа (скорость света в м/с)
-const LIGHT_SPEED = 299792458;
+//4. Создать переменную,
+//которая хранит внутри себя скорость света
 
-function compareSpeed(speed) {
-  if (speed > LIGHT_SPEED) {
-    console.log("Сверхсветовая скорость");
-  } else if (speed < LIGHT_SPEED) {
-    console.log("Субсветовая скорость");
+const SpeedLight = 300000;
+
+function checkSpeed(speed) {
+  if (speed > SpeedLight) {
+    
+    console.log("больше скорости света");
+  } else if (speed < SpeedLight) {
+    console.log("меньше скорости света");
   } else {
     console.log("Скорость света");
   }
 }
+checkSpeed(1);
+checkSpeed(300001);
+checkSpeed(300000);
 
-// Примеры вызова:
-compareSpeed(500);            // Субсветовая скорость
-compareSpeed(300000000);      // Сверхсветовая скорость
-compareSpeed(299792458);      // Скорость света
+//5.Создать переменную №1,
+//которая содержит продукт и переменную №2,
 
-// 5. Создать переменную №1,№2,функцию для проверки
-let carName = "Niva Travel";   // Название товара
-let carPrice = 5000;     // Цена товара
+let product = "Мотоцикл-Ямаха";
+let price = 1000;
 
-// Создаёю функцию для покупки
-function tryToBuy(currentBudget) {
-    // Проверяю, хватает ли денег
-    if (currentBudget >= carPrice) {
-        // Если денег достаточно или впритык
-        console.log(carName + " приобретён. Спасибо за покупку!");
-    } else {
-        // Если денег меньше, чем цена
-        let difference = carPrice - currentBudget;
-        console.log("Вам не хватает " + difference + "$, пополните баланс.");
-    }
-}
-// Проверяю работу программы
-console.log("Проверка покупки");
-
-// есть 200$
-tryToBuy(200); 
-
-// А теперь, есть 400$
-tryToBuy(400);
-
-// 1. Создаю 3 переменные (модели машин) 
-let ladaModel = "Lada Vesta";
-let nivaModel = "Niva Travel";
-let vazModel = "Vaz 2107";
-
-// 2. Создаю функцию
-// Название: checkCarStatus (Проверить статус машины)
-
-function checkCarStatus(carName) {
-    console.log("Проверка состояния:" + carName);
-    console.log(carName + "Готов к отправке!");
+function buyMotik(MoneyHave) {
+  if (MoneyHave >= price) {
+    //Хватило 
+    console.log(product + "Ямаха мой");
+  } else {
+    // Не хватило
+    let NotEnough = price - MoneyHave;
+    console.log("не хватает " + NotEnough + "$");
+  }
 }
 
-// 3. Запускаю функцию для машин
-checkCarStatus(ladaModel);
-checkCarStatus(nivaModel);
-checkCarStatus(vazModel);
+buyMotik(1);  
+buyMotik(10000);
+
+//6. Создать 1 функцию и именовать её
+//7. Создать 3 переменных 
+
+let Moto = "Umaha";
+let Spidde = "200к/м";
+let mileage = "50к/м";
+
+function toKnowProduct() {
+  console.log("Название " + Moto + " Скорость " + Spidde +" Пробег "+ mileage);
+}
+
+  toKnowProduct();
