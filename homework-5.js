67c5be4e0a1e7e99a01363a1bd77d28e12b287a8
devgai34-gif <devgai34@gmail.com>
2026-04-03 10:23:45 +0300

    fix errors

--- homework-5.js
+++ homework-5.js
@@ -1,45 +1,43 @@
 //3. Создать функцию, которая принимает 2 параметра: 
 // город и температуру
 
-function weather(city,temperature) {
-  console.log(" в " + city + " t* — " + temperature + " градус");
+function printValue(cityName,currentTemperature) {
+  console.log(" в " + cityName + " t* — " + currentTemperature + " градус");
 }
 
-weather("Грозном",1);
+printValue("Грозном", 1);
 
 //4. Создать переменную,
 //которая хранит внутри себя скорость света
 
-const SpeedLight = 300000;
-
-function checkSpeed(speed) {
-  if (speed > SpeedLight) {
-    
+const SPEED_LIGHT = 300000;
+function compareSpeed(speed) {
+  if (speed > SPEED_LIGHT) {
     console.log("больше скорости света");
-  } else if (speed < SpeedLight) {
+  } else if (speed < SPEED_LIGHT) {
     console.log("меньше скорости света");
   } else {
     console.log("Скорость света");
   }
 }
-checkSpeed(1);
-checkSpeed(300001);
-checkSpeed(300000);
+
+compareSpeed(1);
+compareSpeed(300001);
+compareSpeed(300000);
 
 //5.Создать переменную №1,
 //которая содержит продукт и переменную №2,
-
 let product = "Мотоцикл-Ямаха";
 let price = 1000;
 
-function buyMotik(MoneyHave) {
-  if (MoneyHave >= price) {
+function buyMotik(moneyHave) {
+  if (moneyHave >= price) {
     //Хватило 
     console.log(product + "Ямаха мой");
   } else {
     // Не хватило
-    let NotEnough = price - MoneyHave;
-    console.log("не хватает " + NotEnough + "$");
+    let notEnough = price - moneyHave;
+    console.log("не хватает " + notEnough + "$");
   }
 }
 
@@ -49,12 +47,12 @@ buyMotik(10000);
 //6. Создать 1 функцию и именовать её
 //7. Создать 3 переменных 
 
-let Moto = "Umaha";
-let Spidde = "200к/м";
+let moto = "Umaha";
+let speed = "200к/м";
 let mileage = "50к/м";
 
 function toKnowProduct() {
-  console.log("Название " + Moto + " Скорость " + Spidde +" Пробег "+ mileage);
+  console.log("Название " + moto + " Скорость " + speed +" Пробег "+ mileage);
 }
 
   toKnowProduct();
