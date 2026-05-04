dd3d71f4fadf86d70bf8539ae234663abbaae9c1
devgai34-gif <devgai34@gmail.com>
2026-05-04 20:12:58 +0300

    fix errors

--- homework-8.js
+++ homework-8.js
@@ -1,60 +1,58 @@
 import { products } from './products.js';
 
-// 4. с .reduce(), получить массив объектов,
-// ключь название продукта,значение - описание
+// 4. С использованием .reduce() получаем массив объектов:
+// ключ — название продукта, значение — описание.
 const productMap = products.reduce((acc, product) => {
-    acc[product.name] = product.subtitle;
+    const newEntry = { [product.name]: product.subtitle };
+    acc.push(newEntry);
     return acc;
-}, {});
+}, []);
+
 console.log("Карта продуктов:", productMap);
 
-//5."Сколько карточек отобразить? От 1 до 5"
-// и +  (проверка if).
 function getCountFromUser() {
     let count = prompt("Сколько карточек отобразить? От 1 до 5");
     count = parseInt(count);
 
     if (isNaN(count) || count < 1 || count > 5) {
-        alert("введите число от 1 до 5!");
-        return null;
+        alert("Ошибка! введите число от 1 до 5.");
+        // РЕКУРСИЯ
+        return getCountFromUser();
     }
     return count;
 }
 
-function renderCards(count) {
-    if (!count) return;
+function renderCards(data) {
+    if (!data || data.length === 0) return;
 
     const list = document.querySelector('.card-list');
-    const templateElement = document.querySelector('.card-container'); 
-    
-    if (!templateElement) return;
+    const templateElement = document.querySelector('.card-container');
+    if (!templateElement || !list) return;
 
     const cardTemplate = templateElement.cloneNode(true);
+    
+    list.innerHTML = '';
 
-   list.innerHTML = '';
-   list.replaceChildren(); 
-
-    const selectedProducts = products.slice(0, count);
-
-    selectedProducts.forEach(item => {
+    data.forEach(item => {
         const newCard = cardTemplate.cloneNode(true);
 
         newCard.querySelector('h2').textContent = item.name;
-        newCard.querySelector('.product-subtitle').textContent = item.subtitle;
+        newCard.querySelector('p').textContent = item.subtitle;
         
         const productImg = newCard.querySelector('img[alt]');
         if (productImg) {
-            productImg.src = item.img;
+            productImg.src = `img-cards/${item.img}.png`;
         }
         
         const priceSpan = newCard.querySelector('.price-block span');
         if (priceSpan) {
-            priceSpan.innerHTML = `${item.price} <span>&#8381;</span>`;
+            priceSpan.innerHTML = `${item.price.toLocaleString()} <span>&#8381;</span>`;
         }
 
         list.appendChild(newCard);
     });
 }
 
-const number = getCountFromUser();
-renderCards(number);
+const count = getCountFromUser();
+const selectedProducts = products.slice(0, count);
+renderCards(selectedProducts);
